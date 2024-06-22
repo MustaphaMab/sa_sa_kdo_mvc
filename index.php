bbf45c1c6790d58cef5bfc9409e67fda6c2b9a33
MustaphaMab <145995419+MustaphaMab@users.noreply.github.com>
2024-06-23 00:22:05 +0200

    création fichiers mvc

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+
+/* --------------------------------- Modèles -------------------------------- */
+require_once('App/Model.php');
+
+require_once('Models/Security.php');
+/* ------------------------------- Controllers ------------------------------ */
+require_once('App/Controller.php');
+
+/* ---------------------------------- Utils --------------------------------- */
+require_once('Utils/header.php');
+
+
+
+
+$controllers=['home', 'user','security', 'admin', 'carts', 'category', 'delivery', 'order','payment','product',];
+$controller_default='home';
+
+if(isset($_GET['controller']) and in_array($_GET['controller'],$controllers))
+{
+    $nom_controller=$_GET['controller'];
+}
+else
+    $nom_controller=$controller_default;
+
+$nom_classe="Controller_".$nom_controller;
+$nom_fichier="Controllers/".$nom_classe.".php";
+
+
+if(file_exists($nom_fichier))
+{
+    require_once("$nom_fichier");
+    $controller=new $nom_classe();
+}
+else 
+    exit("ERROR 404:not found");
+
+// require_once('Utils/footer.php');
+
